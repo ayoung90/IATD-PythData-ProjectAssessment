af1de0445e659d118a3a91f292f73e39ad668f28
ayoung90 <ayoung90@live.com.au>
2023-09-03 21:12:20 +1000

    add unit tests for file load and joined dataframe keys

--- test/test_afl_stats.py
+++ test/test_afl_stats.py
@@ -1,12 +1,47 @@
 import pytest
 from testbook import testbook
+import pdb
+
+# https://seleniumbase.com/the-ultimate-pytest-debugging-guide-2021/
 
 @pytest.fixture(scope='module')
 def tb():
-    with testbook('../afl_stats.ipynb', execute=True) as tb:
+    with testbook('afl_stats.ipynb', execute=True) as tb:
         yield tb
 
-# Test using function call.
-def test_int(tb):
-    int_ref = tb.ref("int_ref")
-    assert int_ref == 1
+#verify that all csvs were loaded and contain data
+def test_csv_load(tb):
+    """ Validate that all files load successfully 
+    Args:
+        tb (testbook): afl_stats jupyter notebook
+    """
+    
+    stats_ref = tb.ref("stats")
+    players_ref = tb.ref("players")
+    games_ref = tb.ref("games")
+
+    #validate files are not empty
+    assert len(stats_ref) > 0
+    assert len(players_ref) > 0
+    assert len(games_ref) > 0
+
+    stats_ref2 = tb.value("stats['gameId'][0]")
+    
+    #init trace
+    #pdb.set_trace()
+    
+    assert type(stats_ref2) == str
+    
+def test_games_stats_players_columns(tb):
+    """ validates that one column from each file exists in joined dataframe
+    Args:
+        tb (testbook): afl_stats jupyter notebook
+    """
+    
+    #extract all keys from joined dataframe
+    key_columns = tb.value("games_stats_players.keys().array.tolist()")
+    
+    #check at least 1 column exists from each file
+    assert key_columns.__contains__("venue") #games
+    assert key_columns.__contains__("height") #players
+    assert key_columns.__contains__("Handballs") #stats
